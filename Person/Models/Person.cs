61b7fa2e4fcd1f2f714a1e645042377eecac77b3
Luiz Araujo <arauldesign@gmail.com>
2025-03-05 20:23:50 -0300

    feature: creating person model

---
+++ Person/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Person.Models
+{
+    public class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; init; }
+        public string Name { get; private set; }
+    }
+}
